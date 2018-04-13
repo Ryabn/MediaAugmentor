4ead86f494ea4446650061df4eb443dad6580e88
Ryan Yang <javathon@gmail.com>
2018-04-12 18:53:01 -0700

    added opencv library

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 package tech.ryanqyang;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
     }
 }
